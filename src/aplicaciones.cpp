3b0e818e99824d3bdd9fd08ee3ff5ccc69bdbac7
Elrorri <roezgonu@gmail.com>
2026-06-23 20:38:49 -0300

    fin, falta arreglar tiempo de cola 5

--- src/aplicaciones.cpp
+++ src/aplicaciones.cpp
@@ -1,5 +1,15 @@
 #include "../include/aplicaciones.h"
+#include "../include/colaDePrioridadPersona.h"
+#include "../include/tablaPersonas.h"
 
 void listarEnOrden(TTablaPersonas t, char** nombres, nat n){
-
+    TColaDePrioridadPersona cola = crearCP(n);
+    for(nat i=0; i<n; i++){
+        insertarEnCP(copiarTPersona(obtenerPersonaDeTTablaPersonas(t, nombres[i])), cola);
+    }
+    while (!estaVaciaCP(cola)){
+        imprimirTPersona(prioritaria(cola));
+        eliminarPrioritaria(cola); 
+    }
+    liberarCP(cola);
 }
